540bc753be1315b0a7f3b7962e69a2654865ed13
jesusamex <romanmalinovsk20004@gmail.com>
2020-08-26 20:11:40 +0300

    not lol

---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Obstacle")
+        {
+            SceneManager.LoadScene("Menu");
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
---
+++ Assets/Scripts/Win.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Win : MonoBehaviour
+{
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Win")
+        {
+            SceneManager.LoadScene("Win");
+        }
+    }
+}
